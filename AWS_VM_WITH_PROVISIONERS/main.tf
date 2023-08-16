077c221fee77128f36fd99506f79f247a2b9e240
Messias Oliveira <dev.messias.2019@gmail.com>
2023-08-15 22:13:52 -0300

    Deploy de VM na AWS com Providers

---
+++ AWS_VM_WITH_PROVISIONERS/main.tf
@@ -0,0 +1,36 @@
+
+terraform {
+  required_version = "> 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.12.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "messias-remote-state"
+    key    = "aws-vm-provisioners/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  default_tags {
+    tags = {
+      owner      = "messias"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "messias-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ AWS_VM_WITH_PROVISIONERS/vm.tf
@@ -0,0 +1,45 @@
+resource "aws_key_pair" "key" {
+  key_name   = "aws_key"
+  public_key = file("./aws-key.pub")
+}
+
+resource "aws_instance" "vm" {
+  ami                         = "ami-0f47fe3e9defb4cbf"
+  instance_type               = "c6a.2xlarge"
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_ip.txt"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./aws-key")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    content     = "public_ip: ${self.public_ip}"
+    destination = "/tmp/public_ip.txt"
+  }
+
+  provisioner "file" {
+    source      = "./test.txt"
+    destination = "/tmp/exemple.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ami: ${self.ami} >> /tmp/ami.txt",
+      "echo private_ip: ${self.private_ip} >> /tmp/private_ip.txt",
+    ]
+  }
+
+  tags = {
+    Name = "vm-terraform"
+  }
+}
