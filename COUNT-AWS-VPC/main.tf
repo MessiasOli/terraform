51549e8e3729d453ed05439914089a03c885c71b
Messias Oliveira <dev.messias.2019@gmail.com>
2023-08-16 21:12:41 -0300

    Count aws vpc

---
+++ COUNT-AWS-VPC/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_version = "> 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.12.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "messias-remote-state"
+    key    = "aws-vpc-count/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  default_tags {
+    tags = {
+      owner      = "messias"
+      managed-by = "terraform"
+    }
+  }
+}
---
+++ COUNT-AWS-VPC/outputs.tf
@@ -0,0 +1,11 @@
+output "subne_0_id" {
+  value = aws_subnet.subnet[0].id
+}
+
+output "subnet_1_id" {
+  value = aws_subnet.subnet[1].id
+}
+
+output "subnet_2_id" {
+  value = aws_subnet.subnet[2].id
+}
